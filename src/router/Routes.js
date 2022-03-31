59432433563d323930b6089d4ab471da0cf50721
jaredwines <jwines760@gmail.com>
2022-03-30 23:28:06 -0700

    init

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Preview from "../views/Preview";
-import HomeLight from "../views/all-home-version/HomeLight";
-import HomeDark from "../views/all-home-version/HomeDark";
-import RtlHomeLight from "../views/all-home-version/RtlHomeLight";
-import RtlHomeDark from "../views/all-home-version/RtlHomeDark";
+import HomeDark from "../views/HomeDark";
 import NotFound from "../views/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -12,11 +8,7 @@ const Routes = () => {
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={Preview} />
-          <Route path="/home-light" component={HomeLight} />
-          <Route path="/home-dark" component={HomeDark} />
-          <Route path="/rtl-home-light" component={RtlHomeLight} />
-          <Route path="/rtl-home-dark" component={RtlHomeDark} />
+          <Route exact path="/" component={HomeDark} />
           <Route component={NotFound} />
         </Switch>
       </Router>
--- src/views/HomeDark.jsx
+++ src/views/HomeDark.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { Link } from "react-router-dom";
-import Home from "../../components/Home";
-import About from "../../components/About";
-import Portfolio from "../../components/Portfolio";
-import News from "../../components/News";
-import Contact from "../../components/Contact";
+import Home from "../components/Home";
+import About from "../components/About";
+import Portfolio from "../components/Portfolio";
+import News from "../components/News";
+import Contact from "../components/Contact";
 
 const HomeDark = () => {
   document.body.classList.add("dark");
@@ -23,121 +23,122 @@ const HomeDark = () => {
               </div>
               {/* END LOGO */}
 
-              <div className="menu">
-                <ul>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/home-run.svg"
-                      alt="homerun"
-                    />
-                    <span className="menu_content">Home</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/avatar.svg"
-                      alt="avatar"
-                    />
-                    <span className="menu_content">About</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/briefcase.svg"
-                      alt="briefcase"
-                    />
-                    <span className="menu_content">Portfolio</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/paper.svg"
-                      alt="paper"
-                    />
-                    <span className="menu_content">News</span>
-                  </Tab>
-                  <Tab>
-                    <img
-                      className="svg"
-                      src="/assets/img/svg/mail.svg"
-                      alt="mail"
-                    />
-                    <span className="menu_content"> Contact</span>
-                  </Tab>
-                </ul>
-              </div>
-              {/* END MENU */}
+                <div className="menu">
+                  <ul>
+                    <Tab>
+                      <img
+                        className="svg"
+                        src="/assets/img/svg/home-run.svg"
+                        alt="homerun"
+                      />
+                      <span className="menu_content">Home</span>
+                    </Tab>
+                    <Tab>
+                      <img
+                        className="svg"
+                        src="/assets/img/svg/avatar.svg"
+                        alt="avatar"
+                      />
+                      <span className="menu_content">Stair Portfolio</span>
+                    </Tab>
+                    <Tab>
+                      <img
+                        className="svg"
+                        src="/assets/img/svg/briefcase.svg"
+                        alt="briefcase"
+                      />
+                      <span className="menu_content">Rail Portfolio</span>
+                    </Tab>
+                    <Tab>
+                      <img
+                        className="svg"
+                        src="/assets/img/svg/paper.svg"
+                        alt="paper"
+                      />
+                      <span className="menu_content">Gate Portfolio</span>
+                    </Tab>
+                    <Tab>
+                      <img
+                          className="svg"
+                          src="/assets/img/svg/paper.svg"
+                          alt="paper"
+                      />
+                      <span className="menu_content">Custom Portfolio</span>
+                    </Tab>
+                    <Tab>
+                      <img
+                        className="svg"
+                        src="/assets/img/svg/mail.svg"
+                        alt="mail"
+                      />
+                      <span className="menu_content"> Contact</span>
+                    </Tab>
+                  </ul>
+                </div>
+                {/* END MENU */}
 
-              <div className="copyright">
-                <p>
-                  &copy; {new Date().getFullYear()} Tokyo <br /> Created by
-                  <a
-                    href="https://themeforest.net/user/ib-themes"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Ib-Themes
-                  </a>
-                </p>
-              </div>
-              {/* END COPYRIGHT */}
-            </div>
-          </div>
-          {/* END LEFT MENU CONTENT */}
-        </TabList>
-        {/* END SIDEBAR TABLIST */}
-
-        {/* START RIGHT PART CONTENT */}
-        <div className="rightpart">
-          <div className="rightpart_in">
-            <div className="tokyo_tm_section">
-              <div className="container">
-                <TabPanel>
-                  <div data-aos="fade-right" data-aos-duration="1200">
-                    <Home />
-                  </div>
-                </TabPanel>
-                {/* END HOME MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <div
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="100"
-                  >
-                    <About />
-                  </div>
-                </TabPanel>
-                {/* END ABOUT MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <Portfolio />
-                </TabPanel>
-                {/* END PORTFOLIO MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <News />
-                </TabPanel>
-                {/* END NEWS MENU TAB CONTENT */}
-
-                <TabPanel>
-                  <div
-                    data-aos="fade-right"
-                    data-aos-duration="1200"
-                    data-aos-delay="200"
-                  >
-                    <Contact />
-                  </div>
-                </TabPanel>
-                {/* END CONTACT MENU TAB CONTENT */}
+                <div className="copyright">
+                  <p>
+                    &copy; {new Date().getFullYear()} Aloha Millworks
+                  </p>
+                </div>
+                {/* END COPYRIGHT */}
               </div>
             </div>
+            {/* END LEFT MENU CONTENT */}
+          </TabList>
+          {/* END SIDEBAR TABLIST */}
+
+          {/* START RIGHT PART CONTENT */}
+          <div className="rightpart">
+            <div className="rightpart_in">
+              <div className="tokyo_tm_section">
+                <div className="container">
+                  <TabPanel>
+                    <div data-aos="fade-right" data-aos-duration="1200">
+                      <Home />
+                    </div>
+                  </TabPanel>
+                  {/* END HOME MENU TAB CONTENT */}
+
+                  <TabPanel>
+                    <div
+                      data-aos="fade-right"
+                      data-aos-duration="1200"
+                      data-aos-delay="100"
+                    >
+                      <About />
+                    </div>
+                  </TabPanel>
+                  {/* END ABOUT MENU TAB CONTENT */}
+
+                  <TabPanel>
+                    <Portfolio />
+                  </TabPanel>
+                  {/* END PORTFOLIO MENU TAB CONTENT */}
+
+                  <TabPanel>
+                    <News />
+                  </TabPanel>
+                  {/* END NEWS MENU TAB CONTENT */}
+
+                  <TabPanel>
+                    <div
+                      data-aos="fade-right"
+                      data-aos-duration="1200"
+                      data-aos-delay="200"
+                    >
+                      <Contact />
+                    </div>
+                  </TabPanel>
+                  {/* END CONTACT MENU TAB CONTENT */}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        {/* END RIGHT PART CONTENT */}
-      </Tabs>
-      {/* END TABS */}
+          {/* END RIGHT PART CONTENT */}
+        </Tabs>
+        {/* END TABS */}
     </>
   );
 };
